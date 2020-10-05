5a70f6987935cca61d7682228172b615ca668310
Shieroz <nghe191@gmail.com>
2020-10-05 15:22:08 -0400

    Initial commit

---
+++ Assets/Shoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletSpawn, bulletPrefab;
+    public float bulletSpeed = 15f;
+
+    public void ShootBullet() {
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = bulletSpawn.transform.position;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+    }
+}
